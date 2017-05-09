df4fc512e03cc29a99724cfc2b8648373452754f
Musta <8664.joan23@fje.edu>
2017-05-09 18:00:28 +0200

    index

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
 	<title>JMFIT Nutrition</title>
 	<meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+   
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- Website CSS style -->
+    <link rel="stylesheet" type="text/css" href="css/registro.css">
 </head>
 <body>
 <!-- nav con login -->
@@ -18,7 +23,7 @@
       <form id="signin" class="navbar-form navbar-right" role="form">
                        <div class="input-group">
                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                       <input id="email" type="email" class="form-control" name="email" value="" placeholder="Usuario">
+                       <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email">
                        </div>
 
                        <div class="input-group">
@@ -27,33 +32,54 @@
                        </div>
 
                        <button type="submit" class="btn btn-primary">Acceder</button>
-                       <br><a href="registro.php">¿No tienes cuenta?</a>
+                       <br><a href="php/login.php">¿No tienes cuenta?</a>
        </form>
     </div>
   </div>
 </nav>
+<!-- slider -->
+<div class="container">
+  <h2>Ofertas</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="IMG/1.jpg" alt="pic1" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="IMG/2.jpg" alt="pic2" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="IMG/3.jpg" alt="pic3" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
 <!-- Footer -->
 <?php
 include("php/footer.php");
 ?>
-=======
-	<title></title>
-	<meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-</head>
-<body>
-
-
-
-<!-- Footer -->
-<?php
-include("footer.php");
-?>
-<script src="js/jquery-3.2.0.min.js"></script>
->>>>>>> origin/master
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>

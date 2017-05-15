3d756bc21057bdf92a278c9e0bcaf57490df0a8e
jAcebo <35202.joan23@fje.edu>
2017-05-15 19:55:27 +0200

    coming

--- index.php
+++ index.php
@@ -1,43 +1,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>JMFIT Nutrition</title>
-	<meta charset="utf-8"/>
+  <title>JMFIT Nutrition</title>
+  <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-   
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+  <link rel="stylesheet" type="text/css" href="css/navbar.css"/>
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
   <!-- Website CSS style -->
     <link rel="stylesheet" type="text/css" href="css/registro.css">
 </head>
-<body>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 <!-- nav con login -->
 <nav class="navbar navbar-default">
 
   <div class="container-fluid">
-  <img src="IMG/titulo-gran.png" class="img-fluid" alt="Logo" width="25%">
-   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
+ 
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <form id="signin" class="navbar-form navbar-right" role="form">
-                       <div class="input-group">
-                       <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                       <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email">
-                       </div>
-
-                       <div class="input-group">
-                       <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                       <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">                                        
-                       </div>
-
-                       <button type="submit" class="btn btn-primary">Acceder</button>
-                       <br><a href="php/login.php">¿No tienes cuenta?</a>
-       </form>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+          <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email">
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+          <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">                                        
+        </div>
+          <button type="submit" class="btn btn-primary">Acceder</button>
+          <br><a href="php/login.php">¿No tienes cuenta?</a>
+      </form>
     </div>
   </div>
+</nav>
+
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <a class="navbar-brand page-scroll" href="#page-top"><img src="logo/titulo-gran1.png" class="img-fluid" alt="Logo" width="25%"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+  
 </nav>
 
 
@@ -80,12 +108,67 @@
     </a>
   </div>
 </div>
+<section id="intro" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Scrolling Nav</h1>
+                    <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
+                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>About Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services Section -->
+    <section id="services" class="services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Services Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Contact Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+
 
 
 <!-- Footer -->
 <?php
 include("php/footer.php");
 ?>
-<script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>

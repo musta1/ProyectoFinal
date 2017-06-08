8d72fe6b587dfef7c22782f32d37168ed9dd76c3
musta1 <8664.joan23@fje.edu>
2017-06-08 13:43:53 +0200

    index new

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>JMFIT Nutrition</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/registro.css">
+</head>
+
+<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img src="logo/titulo-gran1.png" class="img-fluid" alt="Logo" width="25%">
+                    <img src="IMG/paypal1.png" class="img-rounded" alt="Dietas equilibradas" width="100" height="35">
+
+                </a>
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+            </div>
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Contacto</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Dónde estamos</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Quiénes somos</a>
+                    </li>
+                    <li>
+                        <form action="php/index2.proc.php" method="POST" id="signin" class="navbar-form navbar-right" role="form">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email">
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Acceder</button>
+                            <br><a href="php/login.php">¿No tienes cuenta?</a>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Section -->
+    <section id="intro" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="container">
+    <img src="IMG/ofertas.png"> 
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+    <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="IMG/1.jpg" alt="pic1" style="width:100%;">
+            </div>
+
+            <div class="item">
+                <img src="IMG/2.jpg" alt="pic2" style="width:100%;">
+            </div>
+        
+            <div class="item">
+                <img src="IMG/3.jpg" alt="pic3" style="width:100%;">
+            </div>
+        </div>
+
+    <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+               </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>¿Quiénes somos?</h1><br/>
+                    <h4><p class="lead">Nosotros somos un empresa que ayudamos a nuestros clientes a llevar una rutina alimentaria, ayudándoles a empezar una vida sana. Gracias a nuestras dietas semanales nos convertimos en sus coaches nutricionales personalizados. La entrega de los menús se efectúa a domicilio y en un horario a convenir con el cliente, con tal de facilitarle la compra.</p> </h4>
+                    <br/><br/>
+<br/><br/>
+<br/>
+<br/>
+                </div>
+
+<br/>
+<br
+
+
+
+<!-- Inicio javi-->
+<div class="col-md-6">
+<!--<div class="col-lg-6">-->
+<div class="row">
+
+<!-- code start -->
+<div class="twPc-div">
+    <a class="twPc-bg twPc-block"></a>
+
+    <div>
+        <div class="twPc-button">
+            <!-- Twitter Button | you can get from: https://about.twitter.com/tr/resources/buttons#follow -->
+            <a href="https://twitter.com/JesuitesEdu" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false" data-dnt="true">Follow </a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            <!-- Twitter Button -->   
+        </div>
+
+        <a title="Javi Acebo" href="https://twitter.com" class="twPc-avatarLink">
+            <img alt="Javi Acebo" src="IMG/contacto.png" class="twPc-avatarImg">
+        </a>
+
+        <div class="twPc-divUser">
+            <div class="twPc-divName">
+                <a href="https://twitter.com/">Javi Acebo</a>
+            </div>
+            <span>
+                <a href="https://twitter.com/">@<span>javiacebo</span></a>
+            </span>
+        </div>
+
+        <div class="twPc-divStats">
+            <ul class="twPc-Arrange">
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com/mertskaplan" title="140 Tweet">
+                        <span class="twPc-StatLabel twPc-block">Tweets</span>
+                        <span class="twPc-StatValue">140</span>
+                    </a>
+                </li>
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com" title="885 Following">
+                        <span class="twPc-StatLabel twPc-block">Following</span>
+                        <span class="twPc-StatValue">885</span>
+                    </a>
+                </li>
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com" title="1.810 Followers">
+                        <span class="twPc-StatLabel twPc-block">Followers</span>
+                        <span class="twPc-StatValue">1.810</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- code end -->
+</div>
+</div>
+
+
+
+<!-- Sepración-->
+<div class="col-md-6">
+<!--<div class="col-lg-6">-->
+<div class="row">
+
+<!-- code start -->
+<div class="twPc-div">
+    <a class="twPc-bg twPc-block"></a>
+
+    <div>
+        <div class="twPc-button">
+            <!-- Twitter Button | you can get from: https://about.twitter.com/tr/resources/buttons#follow -->
+            <a href="https://twitter.com/JesuitesEdu" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false" data-dnt="true">Follow</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            <!-- Twitter Button -->   
+        </div>
+
+        <a title="Musta Chouati" href="https://twitter.com" class="twPc-avatarLink">
+            <img alt="Musta Chouati" src="IMG/Musta.png" class="twPc-avatarImg">
+        </a>
+
+        <div class="twPc-divUser">
+            <div class="twPc-divName">
+                <a href="https://twitter.com">Musta Chouati</a>
+            </div>
+            <span>
+                <a href="https://twitter.com">@<span>mustachouati</span></a>
+            </span>
+        </div>
+
+        <div class="twPc-divStats">
+            <ul class="twPc-Arrange">
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com" title="15 Tweet">
+                        <span class="twPc-StatLabel twPc-block">Tweets</span>
+                        <span class="twPc-StatValue">15</span>
+                    </a>
+                </li>
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com/mertskaplan/following" title="97652 Following">
+                        <span class="twPc-StatLabel twPc-block">Following</span>
+                        <span class="twPc-StatValue">97652</span>
+                    </a>
+                </li>
+                <li class="twPc-ArrangeSizeFit">
+                    <a href="https://twitter.com/mertskaplan/followers" title="1 Followers">
+                        <span class="twPc-StatLabel twPc-block">Followers</span>
+                        <span class="twPc-StatValue">1</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- code end -->
+</div>
+</div>
+
+
+
+
+<!-- Fin Musta-->
+</div>
+
+
+
+
+            </div>
+        </div>
+    </section>
+    <!-- Services Section -->
+    <section id="services" class="services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>¿Dónde estamos?</h1><br/>
+                </div>
+                <div class="col-md-12">
+                <div><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2995.1392069436756!2d2.1084762108745587!3d41.34932787732805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x26089fc39cb352a3!2sJesu%C3%AFtes+Bellvitge.+Centre+d&#39;Estudis+Joan+XXIII!5e0!3m2!1ses!2ses!4v1494950323686" width="600" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div></div>
+            </div>
+        </div>
+    </section>
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Contacto</h1><br/>
+                    <div class="container">
+    <div class="row text-center">
+        <div class="col-sm-3 col-xs-6 first-box">
+            <h1><span class="glyphicon glyphicon-earphone"></span></h1>
+            <h3>Phone</h3>
+            <p>+34-93-7654321</p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 second-box">
+            <h1><span class="glyphicon glyphicon-home"></span></h1>
+            <h3>Dirección</h3>
+            <p>Av. Mare de Déu de Bellvitge, 100-110, 08907 L'Hospitalet de Llobregat, Barcelona</p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 third-box">
+            <h1><span class="glyphicon glyphicon-send"></span></h1>
+            <h3>E-mail</h3>
+            <p>info@jmfit.com</p><br>
+        </div>
+        <div class="col-sm-3 col-xs-6 fourth-box">
+            <h1><span class="glyphicon glyphicon-leaf"></span></h1>
+            <h3>Web</h3>
+            <p>www.jmfitnutrition.com</p>
+        </div>
+    </div>
+</div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+   
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+
+
+        <!-- Footer -->
+
+        <script type="text/javascript" src="../js/bootstrap.js"></script>
+    </body>
+</html>
